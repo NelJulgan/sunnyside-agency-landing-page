58c474e01cf226b583b11d0f70beba0cb1da15d7
NelJ <32374083+NelJulgan@users.noreply.github.com>
2023-09-05 20:40:11 +0800

    created js functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 import '../css/main.css';
+
+const navbar = document.querySelector('.navbar-nav');
+const toggleBtn = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-link');
+
+const toggleBtnFunc = function () {
+  navbar.classList.toggle('active');
+  toggleBtn.classList.toggle('active');
+};
+
+toggleBtn.addEventListener('click', toggleBtnFunc);
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', toggleBtnFunc);
+}
